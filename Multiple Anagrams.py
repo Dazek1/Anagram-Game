0beac0c3a87def79aa7773a9b28a1f82503082bd
Darius Khan <dazek11@gmail.com>
2022-12-08 22:25:25 +0000

    Add comments to Multiple Anagrams function

--- Multiple Anagrams.py
+++ Multiple Anagrams.py
@@ -3,11 +3,15 @@ import collections
 def get_anagrams(word, word_list):
     return [i for i in word_list if collections.Counter(word) == collections.Counter(i)]
 
+#Have hard coded the word and word length in for testing purposes. Word length is 8 done as counts the new line.
+
 with open("Word_list.txt", "r") as word_list:
     anagrams = get_anagrams("retinas", (word.strip("\n") for word in word_list if len(word) == 8))
 
 print("The word to unscramble is: sntiaer")
 print(f"There are {len(anagrams)} anagrams to guess")
+
+#Keeps going while anagrams still remain in the list of anagrams made above. Also provides a count of the anagrams remaining
 
 while anagrams:
     guess = input("Enter a guess: ")
